25be7bd3a8b58eba6bfd7001da773b0df636bec0
Arshdeep Singh <ar899345@dal.ca>
2022-07-06 14:43:53 -0300

    Base components

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+import { Dashboard } from "./views/Dashboard";
+
+const routes = [
+    {
+      path: '',
+      name: 'Dashboard',
+      element: <Dashboard />,
+    }
+  ];
+  
+export default routes;
